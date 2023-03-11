26d078a2079148e91ce48b6d39e72c21b84bbfea
DESKTOP-FQ830JM\Ryuu <2357622935@qq.com>
2023-03-12 01:40:38 +0800

    clean up code

--- rgdx-core-test/android/src/org/ryuu/rgdx/AndroidLauncher.java
+++ rgdx-core-test/android/src/org/ryuu/rgdx/AndroidLauncher.java
@@ -7,10 +7,10 @@ import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import org.ryuu.rgdx.RGdxApplication;
 
 public class AndroidLauncher extends AndroidApplication {
-	@Override
-	protected void onCreate (Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new RGdxApplication(), config);
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+        initialize(new RGdxApplication(), config);
+    }
 }
--- rgdx-core-test/core/src/org/ryuu/rgdx/RGdxApplication.java
+++ rgdx-core-test/core/src/org/ryuu/rgdx/RGdxApplication.java
@@ -1,20 +1,10 @@
 package org.ryuu.rgdx;
 
 import com.badlogic.gdx.ApplicationListener;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.Body;
-import com.badlogic.gdx.physics.box2d.BodyDef;
-import com.badlogic.gdx.physics.box2d.FixtureDef;
-import com.badlogic.gdx.physics.box2d.PolygonShape;
 import lombok.Getter;
-import org.ryuu.functional.Action;
 import org.ryuu.gdx.ApplicationListenerFactory;
 import org.ryuu.gdx.ApplicationListenerManagement;
 import org.ryuu.gdx.MulticastApplicationListener;
-import org.ryuu.gdx.physics.box2d.Box2dEngine;
-import org.ryuu.gdx.scenes.scene2d.Canvas;
-import org.ryuu.gdx.scenes.scene2d.CanvasManagement;
 
 public class RGdxApplication implements ApplicationListener {
     @Getter
